e24026e0765f3d20bfcf743dc18fc95739426023
Daniel Meireles <daniel.meireles@outlook.com>
2017-05-04 13:32:31 -0300

    Inclusão da barra de energia e diversas alterações estruturais

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 function Sprite(){
-  this.verificador = 0;//Se 0 está em jogo / Se 1 perdeu vida / Se 2 passou de nivel e ganhou 1 vida / Se 3 não foi iniciado
-  this.score = 0;
-  this.scoreLevel = 0;
   this.g = 0;
   this.x = 0;
   this.y = 0;
@@ -11,142 +8,51 @@ function Sprite(){
   this.vy = 0;
   this.altura = 0;
   this.largura = 0;
-  this.vidas = 0;
   this.color = "";
-  this.mover = function(dt){
-    this.vy = this.vy + (this.ay + this.g) * dt;
-    this.vx = this.vx + (this.ax * dt);
-    this.x = this.x + this.vx * dt;
-    this.y = this.y + this.vy * dt;
-  }
-  this.desenhar = function(ctx){
-    if (this.vidas > 0 && this.verificador == 0){
-      ctx.fillStyle = this.color;
-      ctx.fillRect (this.x, this.y, this.largura, this.altura);
-      level.desenharBase();
-      this.scoreLevel = this.scoreLevel - dt;//Calcula o Score do Level
-    }else if (this.vidas == 0){
-      var texto1 = "Game over!";
-      var texto2 = "Seu score foi " + this.score.toFixed() + " pontos";
-      var texto3 = "Tecle enter para reiniciar";
-      textoFormatado(texto1 ,texto2, texto3, "");
-      resetaValores();
-      this.scoreLevel = 0;
-    }else if (this.verificador == 1){
-      var texto1 = "Colidiu!";
-      if (pc.vidas > 1){
-        var texto2 = "Você perdeu 1 vida ficando com " + pc.vidas + " vidas";
-        var texto3 = "Tecle enter para continuar";
-        textoFormatado(texto1 ,texto2, texto3, "");
-      }else{
-        var texto2 = "Você perdeu 1 vida ficando com somente " + pc.vidas + " vida";
-        var texto3 = "Tecle enter para continuar";
-        textoFormatado(texto1 ,texto2, texto3, "");
-      }
-      resetaValores();
-      this.scoreLevel = 0;
-    }else if (this.verificador == 2){
-      var texto1 = "Parabéns!";
-      var texto2 = "Você passou para o level " + level.level;
-      var texto3 = "Você ganhou 1 vida ficando com " + pc.vidas + " vidas";
-      var texto4 = "Tecle enter para continuar";
-      textoFormatado(texto1 ,texto2, texto3, texto4);
-      resetaValores();
-    }else if (this.verificador == 3){
-      var texto1 = "Bem vindo";
-      var texto2 = "Tecle enter para iniciar";
-      textoFormatado(texto1 ,texto2, "", "");
-      resetaValores();
-    }
+}
+
+Sprite.prototype.mover = function(dt){
+  this.vy = this.vy + (this.ay + this.g) * dt;
+  this.vx = this.vx + (this.ax * dt);
+  this.x = this.x + this.vx * dt;
+  this.y = this.y + this.vy * dt;
+}
+
+Sprite.prototype.desenhar = function(ctx){
+  if (vidas > 0 && verificador == 0){
+    ctx.fillStyle = this.color;
+    ctx.fillRect (this.x, this.y, this.largura, this.altura);
   }
 }
-function Level(){
-  this.tamanhoBaseInicial = 0;
-  this.tamanhoBase = 0;
-  this.level = 1;
-  this.baseX = 0;
-  this.baseY = 0;
-  this.desenharBase = function(){
-    this.tamanhoBase = this.tamanhoBaseInicial / (this.level*0.5);
-    if (this.tamanhoBase < pc.largura){
-      this.tamanhoBase = pc.largura;
-    }
-    else if (this.baseX + this.tamanhoBase > eCanvas.width){
-      this.baseX = eCanvas.width - this.tamanhoBase;
-    }
-    ctx.fillStyle = "gold";
-    ctx.fillRect(this.baseX, this.baseY, this.tamanhoBase, 3);
-  }
-}
-function colidiuCom(){
-  if(pc.y > eCanvas.height-13 && pc.x > level.baseX && pc.x < (level.baseX + level.tamanhoBase - pc.largura)) {
-    if(pc.vy < 30 && Math.abs(pc.vx) < 30){//Altera os valores aceitaveis para pouso
-      pc.vidas++;
-      level.level++;
-      level.baseX = Math.random()*eCanvas.width;
-      pc.score = pc.score + pc.scoreLevel;
-      resetaValores();
-      pc.verificador=2;
-    }else{
-      pc.vidas--;
-      resetaValores();
-      pc.verificador = 1;
-    }
-  }else if(pc.x < 0 || pc.y < 0 || pc.x > eCanvas.width-pc.largura || pc.y > eCanvas.height-pc.largura){
-      pc.vidas--;
-      resetaValores();
-      pc.verificador = 1;
-    }
-  }
+
 function resetaValores(){
   pc.x = eCanvas.width/2;
   pc.y = 50;
   pc.vy = 0;
   pc.vx = 0;
 }
-function teclaPressionada(evento){
-  switch (evento.keyCode){
-    case 37:
-      pc.ax = -10;
-      evento.preventDefault();
-      break;
-    case 39:
-      pc.ax = 10;
-      evento.preventDefault();
-      break;
-    case 38:
-      pc.ay = -30;
-      evento.preventDefault();
-      break;
-      case 13:
-        if (pc.vidas == 0){
-          ctx.clearRect(0, 0, eCanvas.width, eCanvas.height);
-          pc.vidas = 3;
-          level.level = 1;
-          level.baseX = Math.random()*eCanvas.width;
-          pc.score = 0;
-          pc.scoreLevel = pc.scoreInicial;
-        }
-        if (pc.verificador == 1 || pc.verificador == 2 || pc.verificador == 3){
-          ctx.clearRect(0, 0, eCanvas.width, eCanvas.height);
-          pc.verificador = 0;
-          pc.scoreLevel = pc.scoreInicial;
-        }
-        evento.preventDefault();
-        break;
+
+function colidiuCom(){
+  if(pc.y > eCanvas.height-13 && pc.x > plataforma.x && pc.x < (plataforma.x + plataforma.largura - pc.largura)) {
+    if(pc.vy < 30 && Math.abs(pc.vx) < 30){//Altera os valores aceitaveis para pouso
+      vidas++;
+      level++;
+      plataforma.x = Math.random()*eCanvas.width;
+      score = score + scoreLevel;
+      resetaValores();
+      verificador = 2;
+    }else{
+      vidas--;
+      resetaValores();
+      verificador = 1;
     }
-}
-function teclaSolta(evento){
-  switch (evento.keyCode){
-    case 37:
-    case 39:
-      pc.ax = 0;
-      break;
-    case 38:
-      pc.ay = 0;
-      break;
+  }else if(pc.x < 0 || pc.y < 0 || pc.x > eCanvas.width-pc.largura || pc.y > eCanvas.height-pc.largura){
+      vidas--;
+      resetaValores();
+      verificador = 1;
+    }
   }
-}
+
 function textoFormatado(texto1, texto2, texto3, texto4){
   ctx.textAlign="center";
   ctx.fillStyle = "red";
@@ -157,4 +63,125 @@ function textoFormatado(texto1, texto2, texto3, texto4){
   ctx.fillText(texto2, eCanvas.width / 2, eCanvas.height / 2 + 20);
   ctx.fillText(texto3, eCanvas.width / 2, eCanvas.height / 2 + 40);
   ctx.fillText(texto4, eCanvas.width / 2, eCanvas.height / 2 + 60);
+}
+
+function telas(){
+  ctx.fillStyle = "white";
+  ctx.font = "0.7em Arial Black";
+  ctx.textAlign="left";
+  ctx.fillText("Vidas: " + vidas, 10, 20);
+  ctx.fillText("Level: " + level, 10, 35);
+  ctx.textAlign="right";
+  ctx.fillText("Score Level: " + scoreLevel.toFixed(), eCanvas.width - 10, 35);
+  ctx.fillText("Score: " + score.toFixed(), eCanvas.width - 10, 20);
+  if (vidas == 0){
+    var texto1 = "Game over!";
+    var texto2 = "Seu score foi " + score.toFixed() + " pontos";
+    var texto3 = "Tecle enter para reiniciar";
+    textoFormatado(texto1 ,texto2, texto3, "");
+    resetaValores();
+    scoreLevel = 0;
+  }else if (verificador == 1){
+    var texto1 = "Colidiu!";
+    if (vidas > 1){
+      var texto2 = "Você perdeu 1 vida ficando com " + vidas + " vidas";
+      var texto3 = "Tecle enter para continuar";
+      textoFormatado(texto1 ,texto2, texto3, "");
+    }else{
+      var texto2 = "Você perdeu 1 vida ficando com somente " + vidas + " vida";
+      var texto3 = "Tecle enter para continuar";
+      textoFormatado(texto1 ,texto2, texto3, "");
+    }
+    resetaValores();
+    scoreLevel = 0;
+  }else if (verificador == 2){
+    var texto1 = "Parabéns!";
+    var texto2 = "Você passou para o level " + level;
+    var texto3 = "Você ganhou 1 vida ficando com " + vidas + " vidas";
+    var texto4 = "Tecle enter para continuar";
+    textoFormatado(texto1 ,texto2, texto3, texto4);
+    resetaValores();
+  }else if (verificador == 3){
+    var texto1 = "Bem vindo";
+    var texto2 = "Tecle enter para iniciar";
+    textoFormatado(texto1 ,texto2, "", "");
+    resetaValores();
+  }
+}
+
+function atualizaValores(){
+  if (verificador == 0 && scoreLevel >= 0){
+    scoreLevel = scoreLevel - dt;//Calcula o Score do Level
+  }
+  if (combustivelTotal <= 0){
+    pc.ax = 0;
+    pc.ay = 0;
+  }
+  combustivel.largura = combustivelTotal;
+  if (combustivel.largura > eCanvas.width/2){
+    combustivel.color = "#2ecc71";
+  } else if (combustivel.largura < eCanvas.width/2 && combustivel.largura > eCanvas.width/4){
+    combustivel.color = "#f1c40f";
+  } else {
+    combustivel.color = "#e74c3c";
+  }
+  if (plataforma.x + plataforma.largura > eCanvas.width){
+    plataforma.x = eCanvas.width - plataforma.largura;
+  }
+  plataforma.largura = eCanvas.width/level;
+  if (plataforma.largura < pc.largura){
+    plataforma.largura = pc.largura;
+  }
+}
+
+addEventListener("keydown", teclaPressionada);
+function teclaPressionada(evento){
+  if (combustivelTotal > 0){
+    switch (evento.keyCode){
+      case 37:
+          pc.ax = -10;
+          combustivelTotal--;
+        evento.preventDefault();
+        break;
+      case 39:
+          pc.ax = 10;
+          combustivelTotal--;
+        evento.preventDefault();
+        break;
+      case 38:
+          pc.ay = -30;
+          combustivelTotal--;
+        evento.preventDefault();
+        break;
+        case 13:
+          if (vidas == 0){
+            vidas = 3;
+            level = 1;
+            plataforma.x = Math.random()*eCanvas.width;
+            score = 0;
+            scoreLevel = scoreInicial;
+            combustivelTotal = eCanvas.width;
+          }
+          if (verificador == 1 || verificador == 2 || verificador == 3){
+            verificador = 0;
+            scoreLevel = scoreInicial;
+            combustivelTotal = eCanvas.width;
+          }
+          evento.preventDefault();
+          break;
+      }
+    }
+}
+
+addEventListener("keyup", teclaSolta);
+function teclaSolta(evento){
+  switch (evento.keyCode){
+    case 37:
+    case 39:
+      pc.ax = 0;
+      break;
+    case 38:
+      pc.ay = 0;
+      break;
+  }
 }
